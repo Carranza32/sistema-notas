7e9ae57d7e1ed13f675bb4782a4fc06359078c37
Mario Ernesto Carranza <mario.carranza@twgroup.cl>
2022-04-22 16:33:02 -0600

    user updated

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,26 +14,52 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        
 
         \DB::table('users')->delete();
-
+        
         \DB::table('users')->insert(array (
-            0 =>
+            0 => 
             array (
                 'id' => 1,
                 'name' => 'Super admin',
                 'email' => 'super@gmail.com',
                 'email_verified_at' => NULL,
-                'password' => bcrypt('secret123'),
+                'password' => '$2y$10$1RRIKAt9LXKwwQw4MIbgiePusxcwUyivzbfcgQj/w2s5Dpxd5.EhK',
                 'two_factor_secret' => NULL,
                 'two_factor_recovery_codes' => NULL,
                 'remember_token' => NULL,
                 'created_at' => '2022-03-26 04:19:55',
                 'updated_at' => '2022-03-26 04:19:55',
             ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Profe',
+                'email' => 'profe@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$1RRIKAt9LXKwwQw4MIbgiePusxcwUyivzbfcgQj/w2s5Dpxd5.EhK',
+                'two_factor_secret' => NULL,
+                'two_factor_recovery_codes' => NULL,
+                'remember_token' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Estudiante',
+                'email' => 'estudiante@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$1RRIKAt9LXKwwQw4MIbgiePusxcwUyivzbfcgQj/w2s5Dpxd5.EhK',
+                'two_factor_secret' => NULL,
+                'two_factor_recovery_codes' => NULL,
+                'remember_token' => NULL,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
         ));
-
-
+        
+        
     }
 }
